06e3d55b0fb3141e2b5785dde8eedf37d32992d6
DipeshMohanty1098 <dipeshmohanty0226@gmail.com>
2021-05-17 19:36:34 +0530

    intial commit

---
+++ js/init.js
@@ -0,0 +1,18 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  });
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+    
